72d3683ed9731e43d65bd777d973f08baeb450fc
Adrián Alcorta Puente <ardillan@MacBook-Air-de-Adrian.local>
2018-01-15 00:04:02 +0100

    Added support for Spanish language

--- src/MaintenanceScreen.php
+++ src/MaintenanceScreen.php
@@ -1,61 +1,68 @@
 <?php 
 
 namespace Luar;
+require __DIR__ . '/Language.php';
 
 class MaintenanceScreen
 {
-	static public function load(array $options) {
+    static public function load(array $options) {
 
-		$enable = $options['enable'];
+        $enable = $options['enable'];
 
-		if ($enable) {
+        if ($enable) {
 
-			if (!empty($options['flag'])) {
-				if (!file_exists($options['flag'])) {
-					return null;
-				}
-			}
+            if ( !empty($options['language']) ) {
+                $language = new Language($options['language']);
+            }else {
+                $language = new Language();
+            }
 
-			$visible_hosts = $options['visible_hosts'];
+            if (!empty($options['flag'])) {
+                if (!file_exists($options['flag'])) {
+                    return null;
+                }
+            }
 
-			if (!empty($options['img_path'])) {
-				$path_img = $options['img_path'];
-			} else {
-				$path_img = false;
-			}
+            $visible_hosts = $options['visible_hosts'];
 
-			if (!empty($options['bgcolor'])) {	
-				$bgcolor = $options['bgcolor'];
-			} else {
-				$bgcolor = "white";
-			}
+            if (!empty($options['img_path'])) {
+                $path_img = $options['img_path'];
+            } else {
+                $path_img = false;
+            }
 
-			if (!empty($options['title'])) {	
-				$title = $options['title'];
-			} else {
-				$title = "Website in maintenance mode";
-			}
+            if (!empty($options['bgcolor'])) {	
+                $bgcolor = $options['bgcolor'];
+            } else {
+                $bgcolor = "white";
+            }
 
-			if (!empty($options['css_path'])) {	
-				$css = $options['css_path'];
-			} else {
-				$css = false;
-			}
+            if (!empty($options['title'])) {	
+                $title = $options['title'];
+            } else {
+                $title = $language->getTitle();
+            }
 
-			if (!empty($options['text'])) {	
-				$text = $options['text'];
-			} else {
-				$text = "The website is under maintenance. Please try again in a few minutes.";
-			}
-			
-			if ($enable && !in_array($_SERVER["HTTP_HOST"], $visible_hosts)) {
-				
-				include_once "screen.php";
-				die();
-			} elseif ($enable && in_array($_SERVER["HTTP_HOST"], $visible_hosts)) {
-				echo "MAINTENANCE ACTIVE. YOUR HOST: ".$_SERVER["HTTP_HOST"]."<br/>";
-			}
+            if (!empty($options['css_path'])) {	
+                $css = $options['css_path'];
+            } else {
+                $css = false;
+            }
 
-		}
-	}
+            if (!empty($options['text'])) {	
+                $text = $options['text'];
+            } else {
+                $text = $language->getMainText();
+            }
+            
+            if ($enable && !in_array($_SERVER["HTTP_HOST"], $visible_hosts)) {
+                
+                include_once "screen.php";
+                die();
+            } elseif ($enable && in_array($_SERVER["HTTP_HOST"], $visible_hosts)) {
+                echo "MAINTENANCE ACTIVE. YOUR HOST: ".$_SERVER["HTTP_HOST"]."<br/>";
+            }
+
+        }
+    }
 }
